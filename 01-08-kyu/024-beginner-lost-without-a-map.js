a485ca236ca1851fe041ca13c9300823bf7ecfad
mengswe <121373899+mengswe@users.noreply.github.com>
2022-12-25 12:46:58 +0000

    Add solution for beginner-lost-without-a-map challenge

---
+++ 01-08-kyu/024-beginner-lost-without-a-map.js
@@ -0,0 +1,16 @@
+/*
+==========
+CHALLENGE
+==========
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+[1, 2, 3] --> [2, 4, 6]
+*/
+
+/*
+=========
+SOLUTION
+=========
+*/
+const maps = x => x.map(num => num * 2);
