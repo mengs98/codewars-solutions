61086c66e428666aad082b40b48a5a104bd40d54
mengswe <121373899+mengswe@users.noreply.github.com>
2022-12-25 07:55:41 +0000

    Add template for challenge & its solutions, also change challenge 1 format

--- 01-08-kyu/001-even-or-odd.js
+++ 01-08-kyu/001-even-or-odd.js
@@ -5,9 +5,17 @@ CHALLENGE
 Create a function that takes an integer as an argumentand returns "Even" for even numbers or "Odd" for odd numbers.
 */
 
-// Solution 1
-// const evenOrOdd = num => num % 2 === 0 ? 'Even' : 'Odd';
+/*
+===========
+SOLUTION 1
+===========
+const evenOrOdd = num => num % 2 === 0 ? 'Even' : 'Odd';
+*/
 
-// Solution 2
-// num % 2 equal to 0 (falsy) for even numbers, so below code will returns 'Even' for every argument that has even number
+/*
+===========
+SOLUTION 2
+===========
+num % 2 equal to 0 (falsy) for even numbers, so below code will returns 'Even' for every argument that has even number.
+*/
 const evenOrOdd = num => num % 2 ? 'Odd' : 'Even';
