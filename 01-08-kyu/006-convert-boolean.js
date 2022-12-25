314f9cb7247da394f35349069269bafdabfb536c
mengswe <121373899+mengswe@users.noreply.github.com>
2022-12-25 08:52:33 +0000

    Add solution for convert boolean challenge

---
+++ 01-08-kyu/006-convert-boolean.js
@@ -0,0 +1,13 @@
+/*
+==========
+CHALLENGE
+==========
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+*/
+
+/*
+=========
+SOLUTION
+=========
+*/
+const boolToWord = bool => bool ? 'Yes' : 'No';
