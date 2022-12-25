5d0d6dd58a0245fc7c5162eee173ffaab5404ce7
mengswe <121373899+mengswe@users.noreply.github.com>
2022-12-25 17:25:23 +0000

    Add solution for dna-to-rna-conversion challenge

---
+++ 01-08-kyu/048-dna-to-rna-conversion.js
@@ -0,0 +1,36 @@
+/*
+==========
+CHALLENGE
+==========
+Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+Create a function which translates a given DNA string into RNA.
+
+For example:
+
+"GCAT"  =>  "GCAU"
+The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+*/
+
+/*
+===========
+SOLUTION 1
+===========
+*/
+const DNAtoRNA = dna => dna.replace(/T/g, 'U');
+
+/*
+===========
+SOLUTION 2
+===========
+const DNAtoRNA = dna => dna.split('').reduce((acc, curr) => acc + (curr === 'T' ? 'U' : curr), '');
+*/
+
+/*
+===========
+SOLUTION 3
+===========
+const DNAtoRNA = dna => dna.split('').map(char => char !== 'T' ? char : 'U').join('');
+*/
