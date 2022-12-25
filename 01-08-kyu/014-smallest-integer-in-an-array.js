afa6571734bfb875cb97e6e3a8c8c689148bad24
mengswe <121373899+mengswe@users.noreply.github.com>
2022-12-25 10:26:41 +0000

    Add solution for smallest-integer-in-an-array challenge

---
+++ 01-08-kyu/014-smallest-integer-in-an-array.js
@@ -0,0 +1,34 @@
+/*
+==========
+CHALLENGE
+==========
+Given an array of integers your solution should find the smallest integer.
+
+For example:
+
+Given [34, 15, 88, 2] your solution will return 2
+Given [34, -345, -1, 100] your solution will return -345
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+*/
+
+/*
+===========
+SOLUTION 1
+===========
+*/
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args);
+  }
+}
+
+/*
+===========
+SOLUTION 2
+===========
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return args.sort((a, b) => a - b)[0];
+  }
+}
+*/
