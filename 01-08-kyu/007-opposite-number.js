85c6a804486e7b040729e8a97149541ccdf622b1
mengswe <121373899+mengswe@users.noreply.github.com>
2022-12-25 08:55:46 +0000

    Add solution for opposite number challenge

---
+++ 01-08-kyu/007-opposite-number.js
@@ -0,0 +1,19 @@
+/*
+==========
+CHALLENGE
+==========
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+*/
+
+/*
+=========
+SOLUTION
+=========
+*/
+const opposite = num => -(num);
