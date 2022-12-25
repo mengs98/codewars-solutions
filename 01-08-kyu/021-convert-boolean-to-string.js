d45f5ea8bc356ef4d56f031039cad134f76f8d73
mengswe <121373899+mengswe@users.noreply.github.com>
2022-12-25 12:21:56 +0000

    Add solution for convert-boolean-to-string challenge

---
+++ 01-08-kyu/021-convert-boolean-to-string.js
@@ -0,0 +1,36 @@
+/*
+==========
+CHALLENGE
+==========
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+*/
+
+/*
+===========
+SOLUTION 1
+===========
+*/
+const booleanToString = b => `${b}`;
+
+/*
+===========
+SOLUTION 2
+===========
+const booleanToString = b => b.toString();
+*/
+
+/*
+===========
+SOLUTION 3
+===========
+const booleanToString = b => String(b);
+*/
+
+/*
+===========
+SOLUTION 4
+===========
+const booleanToString = b => b + '';
+*/
