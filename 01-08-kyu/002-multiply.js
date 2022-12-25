53eec9a1fafae3cfeed9030b7c19a8f7c4fda96f
mengswe <121373899+mengswe@users.noreply.github.com>
2022-12-25 08:01:01 +0000

    Add solution for multiply challenge

---
+++ 01-08-kyu/002-multiply.js
@@ -0,0 +1,29 @@
+/*
+==========
+CHALLENGE
+==========
+This code does not execute properly. Try to figure out why!
+
+function multiply(a, b){
+  a * b
+}
+*/
+
+/*
+===========
+SOLUTION 1
+===========
+The function needs to return something. Add keyword 'return' in the function body
+
+function multiply(a, b) {
+  return a * b
+}
+*/
+
+/*
+===========
+SOLUTION 2
+===========
+With arrow function, we don't need to explicitly write 'return' keyword for returning value
+*/
+const multiply = (a, b) => a * b;
