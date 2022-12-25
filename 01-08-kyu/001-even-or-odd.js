e931e4552fc5d751bd5fc06cdac974d9748eb0d4
mengswe <121373899+mengswe@users.noreply.github.com>
2022-12-25 07:49:30 +0000

    Add solution for even or odd challenge

---
+++ 01-08-kyu/001-even-or-odd.js
@@ -0,0 +1,13 @@
+/*
+==========
+CHALLENGE
+==========
+Create a function that takes an integer as an argumentand returns "Even" for even numbers or "Odd" for odd numbers.
+*/
+
+// Solution 1
+// const evenOrOdd = num => num % 2 === 0 ? 'Even' : 'Odd';
+
+// Solution 2
+// num % 2 equal to 0 (falsy) for even numbers, so below code will returns 'Even' for every argument that has even number
+const evenOrOdd = num => num % 2 ? 'Odd' : 'Even';
