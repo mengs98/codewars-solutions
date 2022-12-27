4ea98068108bd45b433317909355995d234f2bc9
mengswe <121373899+mengswe@users.noreply.github.com>
2022-12-27 23:44:58 +0000

    Add solution for remove-exclamation-marks challenge

---
+++ 01-08-kyu/055-remove-exclamation-marks.js
@@ -0,0 +1,13 @@
+/*
+==========
+CHALLENGE
+==========
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+*/
+
+/*
+=========
+SOLUTION
+=========
+*/
+const removeExclamationMarks = s => s.replace(/!/g, '');
