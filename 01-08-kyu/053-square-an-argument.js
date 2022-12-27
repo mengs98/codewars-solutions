b80b0dadd72cf566cee281673995bb319a55a457
mengswe <121373899+mengswe@users.noreply.github.com>
2022-12-27 23:26:50 +0000

    Add solution for square-an-argument challenge

---
+++ 01-08-kyu/053-square-an-argument.js
@@ -0,0 +1,13 @@
+/*
+==========
+CHALLENGE
+==========
+Now you have to write a function that takes an argument and returns the square of it.
+*/
+
+/*
+=========
+SOLUTION
+=========
+*/
+const square = num => num ** 2;
