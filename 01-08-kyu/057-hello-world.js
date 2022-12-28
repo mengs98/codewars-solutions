3f5f99dc41cef88270e4b30110746b98b5fc325c
mengswe <121373899+mengswe@users.noreply.github.com>
2022-12-28 00:00:06 +0000

    Add solution for hello-world challenge

---
+++ 01-08-kyu/057-hello-world.js
@@ -0,0 +1,17 @@
+/*
+==========
+CHALLENGE
+==========
+Make a hello world function that doesn't return anything, only log it.
+*/
+
+/*
+=========
+SOLUTION
+=========
+*/
+function helloWorld() {
+  const str = "Hello World!";
+    
+  console.log(str);
+}
