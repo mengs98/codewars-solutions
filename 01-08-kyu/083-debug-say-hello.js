d04ee8f6b2179ebb37c392b924b5a623635444a9
mengswe <121373899+mengswe@users.noreply.github.com>
2022-12-31 09:57:37 +0000

    Add solution for debug-say-hello challenge

---
+++ 01-08-kyu/083-debug-say-hello.js
@@ -0,0 +1,21 @@
+/*
+==========
+CHALLENGE
+==========
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+Fix this function:
+function sayHello(name) {
+  return 'Hello'
+}
+
+Example output:
+Hello, Mr. Spock
+*/
+
+/*
+=========
+SOLUTION
+=========
+*/
+const sayHello = name => `Hello, ${name}`;
