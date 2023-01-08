81d1282d2eeb83e69d97331ea06d9cb70670cd35
mengswe <121373899+mengswe@users.noreply.github.com>
2023-01-08 18:41:50 +0000

    Add solution for unique-in-order challenge

---
+++ 03-06-kyu/011-unique-in-order.js
@@ -0,0 +1,31 @@
+/*
+==========
+CHALLENGE
+==========
+mplement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+For example:
+
+uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+*/
+
+/*
+=========
+SOLUTION
+=========
+*/
+const uniqueInOrder = (iterable) => {
+  let result = [];
+    
+  for(let i = 0; i < iterable.length; i++) {
+    if(i === 0) {
+      result.push(iterable[i]);
+    } else {
+      iterable[i] !== result[result.length - 1] && result.push(iterable[i]);
+    }
+  }
+    
+  return result;
+}
